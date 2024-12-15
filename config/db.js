c2bdb5825eea89f4761e4cfe07fbacc2d79ebce9
yuzopluzo02 <96601245+yuzopluzo02@users.noreply.github.com>
2024-12-15 11:56:07 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'TaylorSwift55443',
+  password: '1234',
   database: 'online_store',
 });
 
